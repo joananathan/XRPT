0b3956ba9835f0c497f41c526cfece61f8a5405d
Jonathan Zhu <jonathan.zhu5@education.nsw.gov.au>
2025-05-06 00:33:30 +1000

    form ui finished: added the video style filter dropdown options and submit button styled

--- filter.py
+++ filter.py
@@ -1,8 +1,13 @@
 from moviepy import VideoFileClip
 from moviepy import vfx, afx
 
-myclip = VideoFileClip("clip_0.mp4")
+myclip = VideoFileClip("clip_1 - Trim.mp4")
 
-myclip = myclip.with_effects([vfx.MultiplyColor(0.5)])
+# myclip = myclip.with_effects([vfx.Painting(1.9, 2)])
+# myclip = myclip.with_effects([vfx.LumContrast(lum=160, contrast=2.5, contrast_threshold=120)])
+myclip = myclip.with_effects([vfx.MirrorX()])
+
+
+
 
 myclip.write_videofile("output.mp4", codec="libx264")
